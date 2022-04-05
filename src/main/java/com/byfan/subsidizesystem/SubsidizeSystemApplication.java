a7f23373d8c8805c15152f2f9c682295c188cdc8
byfan <3257490363@qq.com>
2022-04-05 15:11:21 +0800

    初始化项目

---
+++ src/main/java/com/byfan/subsidizesystem/SubsidizeSystemApplication.java
@@ -0,0 +1,13 @@
+package com.byfan.subsidizesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubsidizeSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubsidizeSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/byfan/subsidizesystem/SubsidizeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byfan.subsidizesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubsidizeSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
